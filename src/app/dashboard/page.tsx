a544a8fae0815476a7c9e0dfe675e62616e086b9
pedrostyle <pedrostyle@gmail.com>
2025-04-16 22:08:34 -0300

    :art: style: create dashboard page and navlinks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,34 @@
+'use client';
+
+import { Pen, UserCirclePlus, UsersThree } from 'phosphor-react'
+import styles from './dashboard.module.css'
+import Link from 'next/link';
+
 export default function Dashboard() {
     return (
-        <h1>PÁGINA x</h1>
+        <section className={styles.container}>
+            <nav className={styles.navWrapper}>
+                <ul>
+                    <li>
+                        <Link href="/cadastrar">
+                            <UserCirclePlus size={48} />
+                            <span>Cadastrar</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/editar">
+                            <Pen size={48} />
+                            <span>Editar Cadastro</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/listar">
+                            <UsersThree size={48} />
+                            <span>Listar</span>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </section>
     )
 }

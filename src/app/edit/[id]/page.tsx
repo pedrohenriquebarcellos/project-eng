ecad7b70f2ad986853f707a99427331b6bb62913
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-05-08 08:26:52 -0300

    :ambulance: feat: add error logs to edit page catch

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -5,16 +5,21 @@ import { api } from "@/lib/axios";
 import { Company } from "@/app/components/GetCompanies";
 import CompanyForm from "@/app/components/CompanyForm";
 
-export default async function CompanyDetailsPage({ params }: { params: { id: string } }) {
-    const { id } = params;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
 
     try {
         const response = await api.get(`/companies/${id}`);
         const company: Company = response.data;
 
         return <CompanyForm company={company} />;
-    } catch (error) {
-        console.error("Erro ao buscar empresa:", error);
-        return notFound();
+    } catch (error: any) {
+        console.error("Erro ao buscar empresa:", {
+            message: error.message,
+            code: error.code,
+            status: error.response?.status,
+            data: error.response?.data,
+            headers: error.response?.headers,
+        });
     }
 }

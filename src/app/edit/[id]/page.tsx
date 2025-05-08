5e7d7fb092778e04ded5031a7c9d590416679b16
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-05-08 08:54:57 -0300

    :construction: feat: add other values to test

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -30,5 +30,28 @@ export default function CompanyDetailsPage() {
     if (error) return <div>Empresa não encontrada</div>;
     if (!company) return <div></div>;
 
+    return (
+        <div>
+            <h1>Detalhes da Empresa</h1>
+            <p>ID: {company.id}</p>
+            <p>CNPJ: {company.cnpj}</p>
+            <p>Nome Fantasia: {company.companyFantasyName}</p>
+            <p>Razão Social: {company.companyLegalName}</p>
+            <p>Endereço: {company.companyAddressStreet}</p>
+            <p>Bairro: {company.companyAddressDistrict}</p>
+            <p>Número da Loja: {company.companyStoreIdNumber}</p>
+            <p>Tipo: {company.companyType}</p>
+            <p>CEP: {company.companyCEP}</p>
+            <p>Estado: {company.companyState}</p>
+            <p>Cidade: {company.companyCity}</p>
+            <p>Região: {company.companyRegion || 'Não informado'}</p>
+            <p>País: {company.companyCountryDescription}</p>
+            <p>Código do País: {company.companyCountryId}</p>
+            <p>Telefone: ({company.companyPhoneCode}) {company.companyPhone}</p>
+            <p>Data de Abertura: {company.companyBirthDate || 'Não informado'}</p>
+            <p>Site: {company.companyHomePage || 'Não informado'}</p>
+        </div>
+    )
+
     return <CompanyForm company={company} />;
 }

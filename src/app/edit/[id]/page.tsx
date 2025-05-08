b652ac62e9953c5d723483600fed2e7917196742
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-05-08 14:00:05 -0300

    :construction: art: add spinner

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,18 +1,67 @@
-import { notFound } from "next/navigation";
+'use client'
+
+import styles from "./editCompany.module.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { api } from "@/lib/axios";
 import { Company } from "@/app/components/GetCompanies";
 import CompanyForm from "@/app/components/CompanyForm";
+import { notFound } from "next/navigation";
+import { Spinner } from "phosphor-react";
+import { toast } from "react-toastify";
 
-export default async function CompanyDetailsPage({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
+export default function CompanyDetailsPage({ params }: { params: Promise<{ id: string }> }) {
+    const router = useRouter();
+    const [company, setCompany] = useState<Company | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [id, setId] = useState<string | null>(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    try {
-        const response = await api.get(`/companies/${id}`);
-        const company: Company = response.data;
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {            
+            toast.error("Você precisa estar logado para acessar essa página.");
+            router.push("/");
+        } else {
+            setIsAuthenticated(true);
+        }
+    }, [router]);
 
-        return <CompanyForm company={company} />;
-    } catch (error) {
-        console.error("Erro ao buscar empresa:", error);
-        return notFound();
+    useEffect(() => {
+        params.then(({ id }) => setId(id));
+
+        if (id && isAuthenticated) {
+            const fetchCompany = async () => {
+                try {
+                    const response = await api.get(`/companies/${id}`);
+                    setCompany(response.data);
+                    setLoading(false);
+                } catch (error) {
+                    console.error("Erro ao buscar empresa:", error);
+                    setError("Erro ao buscar empresa.");
+                    setLoading(false);
+                }
+            };
+            fetchCompany();
+        }
+    }, [id, params, isAuthenticated]);
+
+    if (loading) {
+        return (
+            <div className={styles.overlay}>
+                <Spinner size={48} className={styles.spinner} />
+            </div>
+        )
     }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    if (company) {
+        return <CompanyForm company={company} />;
+    }
+
+    return notFound();
 }

8f193f7cc07c13bd23c8f10a262e0510d0ab4a45
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-05-15 18:16:08 -0300

    :construction: chore: update metadata

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,39 +1,41 @@
 import axios from "axios";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 
 export const api = axios.create({
-    //baseURL: 'https://projeto-eng-api.onrender.com'
-    baseURL: 'http://localhost:5000'
-})
+  //baseURL: 'https://projeto-eng-api.onrender.com'
+  baseURL: "http://localhost:5000",
+});
 
 export const cnpjApi = axios.create({
-    baseURL: 'https://publica.cnpj.ws'
-})
+  baseURL: "https://publica.cnpj.ws",
+});
 
 api.interceptors.request.use((config) => {
-    if (typeof window !== 'undefined') {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+  }
 
-    return config;
+  return config;
 });
 
 api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error.response?.status === 401) {
-        toast.error('Você precisa estar logado para acessar essa funcionalidade.')
-        localStorage.removeItem('token')
-        localStorage.removeItem('userName')
-  
-        if (typeof window !== 'undefined') {
-          window.location.href = '/login'
-        }
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      toast.error(
+        "Você precisa estar logado para acessar essa funcionalidade."
+      );
+      localStorage.removeItem("token");
+      localStorage.removeItem("userName");
+
+      if (typeof window !== "undefined") {
+        window.location.href = "/login";
       }
-  
-      return Promise.reject(error)
     }
-  )
+
+    return Promise.reject(error);
+  }
+);
